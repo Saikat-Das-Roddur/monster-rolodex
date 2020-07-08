08a5ccb833a37e9a12e6be8e8752048d85caf8be
Saikat-Das-Roddur <saikatd397@gmail.com>
2020-07-08 18:17:01 +0600

    monster added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CardList } from './component/card-list/card-list.coponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {monsters:[1,2,3,4,5,6,7,8,9,10]}
+  }
+
+  
+
+  render(){
+    const  monsterList = this.state.monsters.map(monster => <h1>{monster}</h1>)
+    return (
+      <div className="App">
+      <CardList monster = {monsterList}  />
+      </div>
+    );
+  }
+
 }
 
 export default App;
